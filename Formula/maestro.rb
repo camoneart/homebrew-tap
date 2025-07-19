0362276713579e80f8bb272b8e9aaaa131fa8e95
aoyamadev <106678583+bojjisage@users.noreply.github.com>
2025-07-19 22:35:43 +0900

    maestro 2.0.1
    Fix Homebrew installation by including shell completion files

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,8 +1,8 @@
 class Maestro < Formula
   desc "Orchestrate parallel development with Git Worktrees - supercharge your workflow with Claude Code integration"
   homepage "https://github.com/hashiramaendure/maestro"
-  url "https://registry.npmjs.org/@hashiramaendure/maestro/-/maestro-2.0.0.tgz"
-  sha256 "8ac60be44f9bbd4442da7703460bc69ad18eae9012257ed7488e22574c730df2"
+  url "https://registry.npmjs.org/@hashiramaendure/maestro/-/maestro-2.0.1.tgz"
+  sha256 "19e0d78861f8e14848754d3f91b13a90d7f3874b4a87e7459783782cc3946d04"
   license "MIT"
 
   depends_on "node"
